9014a0dc594068ca56dad54c73c618e0d6d6d2f5
테스트 <test@gmail.com>
2023-08-06 21:16:46 +0900

    Initial commit

---
+++ src/main/java/com/project/waglewagle/WaglewagleApplication.java
@@ -0,0 +1,13 @@
+package com.project.waglewagle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaglewagleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaglewagleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/waglewagle/WaglewagleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.waglewagle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaglewagleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

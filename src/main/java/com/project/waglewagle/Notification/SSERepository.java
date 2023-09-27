8039906f4d1d6d2df8dee53127d1e59d513dd189
bgseong <qnrms2898@naver.com>
2023-09-27 16:46:40 +0900

    feat : redis, 알림기능 구현 중

---
+++ src/main/java/com/project/waglewagle/Notification/SSERepository.java
@@ -0,0 +1,21 @@
+package com.project.waglewagle.Notification;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Map;
+
+public interface SSERepository {
+
+
+    SseEmitter save(String id, SseEmitter sseEmitter);
+
+    void saveEventCache(String id, Notification event);
+    Map<String, SseEmitter> findAllStartById(String id);
+
+    Map<String, Object> findAllEventCacheStartWithId(String id);
+    void deleteAllStartByWithId(String id);
+
+
+
+}
---
+++ src/main/java/com/project/waglewagle/global/config/RedisConfig.java
@@ -0,0 +1,50 @@
+package com.project.waglewagle.global.config;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.cache.redis.host}")
+    private String host;
+    @Value("${spring.cache.redis.port}")
+    private int port;
+    @Value("${spring.cache.redis.password}")
+    private String password;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(password);
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+        return lettuceConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
+
+        return redisTemplate;
+    }
+
+
+}
